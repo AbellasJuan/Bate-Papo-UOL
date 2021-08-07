5e429cbc82e60c10f7de7c29f4242a0bf26e3072
AbellasJuan <juanvictoraa@gmail.com>
2021-08-07 19:24:29 -0300

    projeto realizado

--- scripts.js
+++ scripts.js
@@ -1,45 +1,37 @@
-
 const URL_MESSAGES = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages";
 const URL_PARTICIPANTS = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants";
 const URL_STATUS = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/status";
 
 let nickName;
 
-
-buscarDados();
 entrarComNome()
+buscarDados();
 
   function entrarComNome(){
     nickName = prompt('Qual o seu lindo nickname?');
-
     const data = {
       name: nickName
     };
-
     const promise = axios.post(URL_PARTICIPANTS, data);
     promise.then(tratarSucesso);
     promise.catch(tratarErro);
   }
 
   function manterLogado(){
-
     const name = {
       name: nickName
     }
-
     const promise = axios.post(URL_STATUS, name);
     promise.then(tratarSucesso);
   }setInterval(manterLogado, 5000);
 
   function tratarErro(resposta){
-    
     if (resposta.response.status === 400){
      nickName = prompt("Usuário já existe! \nInsira outro nickname lindão:\n");
                    
      const data = {
       name: nickName
     };
-
     const promise = axios.post(URL_PARTICIPANTS, data);
     promise.then(tratarSucesso);
     promise.catch(tratarErro);
@@ -55,24 +47,37 @@ entrarComNome()
       const promise = axios.get(URL_MESSAGES);
       promise.then(renderizarMensagens);    
     }
-    
     setInterval(buscarDados, 3000);
         
     function renderizarMensagens(resposta){
       let corpoDoChat = document.querySelector(".messages");
+      let mensagem;
       corpoDoChat.innerHTML = " ";
+      
       for(let i=0; i<resposta.data.length; i++){
-  
+        
         let time = resposta.data[i].time;
         let from = resposta.data[i].from;
         let to = resposta.data[i].to;
         let text = resposta.data[i].text;
       
-        const mensagem = `<p><time>(${time})</time> 
+        if(resposta.data[i].type === "status"){
+          mensagem = `<p class="fundo-cinza"><time>(${time})</time> 
+        <strong class="from">&nbsp${from}</strong>&nbsp${text}</p>`
+
+        } if (resposta.data[i].type === "message"){
+          mensagem = `<p class="fundo-branco"><time>(${time})</time> 
         <strong class="from">&nbsp${from}</strong>&nbsppara&nbsp 
         <strong class="to">${to}:&nbsp&nbsp</strong>
         ${text}</p>`
 
+        } if (resposta.data[i].type === "private_message" && resposta.data[i].to === nickName){
+          mensagem = `<p class="fundo-rosa"><time>(${time})</time> 
+        <strong class="from">&nbsp${from}</strong>&nbspreservadamente para&nbsp 
+        <strong class="to">${to}:&nbsp&nbsp</strong>
+        ${text}</p>`
+        } else{
+        }
         corpoDoChat.innerHTML += mensagem  
       }
       scrollAteFinal();
@@ -84,3 +89,31 @@ entrarComNome()
   pegarUltima.scrollIntoView();
   }
 
+
+  function enviarMensagem() {
+    const minhaMensagem = document.querySelector("input").value;
+  
+    const mensagem = {
+      from: nickName,
+      to: "Todos",
+      text: minhaMensagem,
+      type: "message" // ou "private_message" para o bônus
+    }
+  
+    let campoInput = document.querySelector("input");
+    campoInput.value = "";  
+    
+    const promise = axios.post(URL_MESSAGES, mensagem);
+        
+    promise.then(tratarSucessoResposta);
+    promise.catch(tratarErroResposta);
+  }
+
+  function tratarSucessoResposta(enviada){
+    if(enviada.status === 200){
+      buscarDados();
+    }
+  }
+  function tratarErroResposta(naoEnviada){
+    document.location.reload(true);
+  }
